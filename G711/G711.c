dbc5f8b55478d352a2e98b0dd3188174fc9f8346
mihuaz2Z <mihuayouxi@163.com>
2017-07-01 17:54:05 +0800

    upload source code

---
+++ G711/G711.c
@@ -0,0 +1,9 @@
+//
+//  G711.c
+//  G711
+//
+//  Created by Ruiwen Feng on 2017/6/1.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#include "G711.h"
---
+++ G711/G711.h
@@ -0,0 +1,14 @@
+//
+//  G711.h
+//  G711
+//
+//  Created by Ruiwen Feng on 2017/6/1.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#ifndef G711_h
+#define G711_h
+
+#include <stdio.h>
+
+#endif /* G711_h */
